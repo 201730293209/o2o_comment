193ddbb7a5ce611364497b60fba4dffe85cc7b10
Your Name <email@exampl.com>
2019-10-21 20:00:57 +0800

    yyq文件上传

--- yyq.py
+++ yyq.py
@@ -1,1 +1,14 @@
-print("yyq")
+import jieba
+import csv
+import pandas as pd
+
+def textProcess():
+    fR = open('train.txt', 'r', encoding='UTF-8')
+    label = []
+    df=pd.read_csv('train.txt',sep='\t')
+    print(df)
+    df['comment']
+    print(df['comment'])
+
+if __name__=='__main__':
+    textProcess()
