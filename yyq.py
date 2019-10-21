23b22f89da0f4ed843ceb0ffae41e8086ed3c7cf
Your Name <email@exampl.com>
2019-10-21 20:46:47 +0800

    分词尝试

--- yyq.py
+++ yyq.py
@@ -3,12 +3,17 @@ import csv
 import pandas as pd
 
 def textProcess():
-    fR = open('train.txt', 'r', encoding='UTF-8')
-    label = []
-    df=pd.read_csv('train.txt',sep='\t')
-    print(df)
-    df['comment']
-    print(df['comment'])
+    #切分数据集，将label和comment分开
+    trainSet=pd.read_csv('train.csv',sep='\t')
+    trainSet['comment']
+    trainSet=['label']
+
+    #对comment做分割
+    word_cut=jieba.cut(comment,cut_all=False)
+    word_list=list(word_cut)
+    data_list=[]
+    data_list.append(word_list)
+    print(data_list)
 
 if __name__=='__main__':
     textProcess()
