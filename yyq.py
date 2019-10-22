6979e4a875c137912b63dc9208769a93938fe14c
yyqlovelife <50950108+yyqlovelife@users.noreply.github.com>
2019-10-22 14:41:31 +0800

    更新代码

--- yyq.py
+++ yyq.py
@@ -2,71 +2,71 @@ import numpy as np
 import jieba
 import pandas as pd
 
-
-def loadcomment_set():
+def loadcomment_list():
     '''
     函数说明：加载数据
     :return: 
     '''
-    fr = open("train.csv", 'r', encoding='UTF-8')
-    comment_set = []
-    comment_split = []
+    fr = open("C:\\Users\\lin78\\Desktop\\CCF\\train.txt", 'r', encoding = 'UTF-8')
+    stopwords = open("C:\\Users\\lin78\\Desktop\\CCF\\stopwords_cn.txt", 'r', encoding = 'UTF-8')
+    comment_list = []
     label = []
     for line in fr.readlines():
-        lineArr = line.strip().split()#对每一行通过空格和回车进行划分
-        label.append(lineArr[0])#将0,1值打入label数组中
-        comment_set.append(lineArr[1])#将评论语句打入数组中
+        lineArr = line.strip().split()
+        label.append(lineArr[0])
+        string = "/".join(jieba.cut(lineArr[1], cut_all = False))
+        comment_list.append(string.split("/"))
     fr.close()
-    del (label[0])#将第一行的单词‘label’删除
-    del (comment_set[0])#将第一行的单词‘comment’删除
+    del(label[0])
+    del(comment_list[0])
+    return comment_list, label
 
+def sort_by_frequency(comment_list):
     '''
-    用jieba将comment分词，放入到comment_split数组中
+    函数说明：根据词频降序排列每个词
+    :param comment_list: 评论列表
+    :return: all_words_list: 降序排列的所有词
     '''
-    for line in comment_set:
-        string = "/".join(jieba.cut(line, cut_all=False))
-        comment_split.append(string.split('/'))
-
-    '''
-    对切分好之后的词组进行排序
-    '''
-    all_words_dict = {}  # 用于统计训练集词频
-    for word_list in comment_split:
-        for word in word_list:
-            if word in all_words_dict.keys():
-                all_words_dict[word] += 1
+    all_words_dic = {} #所有词的字典
+    for comment in comment_list:  #将每个词对应出现的频率加到字典中
+        for word in comment:
+            if word in all_words_dic.keys():
+                all_words_dic[word] += 1
             else:
-                all_words_dict[word] = 1
-    # 根据键的值倒序排序
-    all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f: f[1], reverse=True)
-    all_words_list, all_words_nums = zip(*all_words_tuple_list)  # 解压缩
-    all_words_list = list(all_words_list)  # 转换成列表
+                all_words_dic[word] = 1
+    all_words_tuple_list = sorted(all_words_dic.items(), key = lambda s: s[1],
+                                  reverse = True) #用key指定按字典的值降序排列
+    all_words_list, all_words_nums = zip(*all_words_tuple_list)  #解压，返回两个元组
+    all_words_list = list(all_words_list)
+    return all_words_list
 
-    return comment_split, label, all_words_list
 
-
-def split_word(text, stopwords):
-    word_list = jieba.cut(text)
-    start = True
-    result = ''
-    for word in word_list:
-        word = word.strip()
-        if word not in stopwords:
-            if start:
-                result = word
-                start = False
-            else:
-                result += ' ' + word
-    return result.encode('utf-8')
-
+def delete_words(all_words_list, delete_num = 100):
+    '''
+    函数说明：文本清洗，去除高频词，数字，停用词
+    :param all_words_list: 所有词的列表
+    :param delete_num: 删除的高频词数目，默认100，需要通过观察确定最好的数目
+    :return: feature_words: 特征词，即没有被清洗的词
+    '''
+    #----------------加载停用词文件数据----------------------
+    fr = open("C:\\Users\\lin78\\Desktop\\CCF\\stopwords_cn.txt", 'r', encoding= 'UTF-8')
+    stopwords_set = set() #使用set去重，虽然没有必要
+    for line in fr.readlines():
+        stopword = line.strip()
+        if len(stopword) > 0:
+            stopwords_set.add(stopword)
+    #-------------------------------------------------------
+    feature_words = []  #特征词，即有效的词
+    for t in range(delete_num, len(all_words_list), 1):
+        # 如果这个词不是数字，并且不是停用词，且单词长度大于1小于5，那么这个词就可以作为特征词
+        if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(all_words_list[t]) < 5:
+            feature_words.append(all_words_list[t])
+    return feature_words
 
 if __name__ == '__main__':
-    comment_split, label,all_words_list = loadcomment_set()
-    for line in range(0,10000):
-        print(label[line],' ',comment_split[line],'\n')
-        #print(comment_split[line])
-
-    print("按词频降序展示：",'\n',all_words_list)
-    #print(comment_split[0])
-    #print("comment_split_sorted:\n", all_words_list)
-    #print("label:\n", label)
+    comment_list, label = loadcomment_list()
+    all_words_list = sort_by_frequency(comment_list)
+    feature_words = delete_words(all_words_list)
+    print("feature_words:\n", feature_words)
+    print("comment_list:\n", comment_list)
+    print("label:\n", label)
