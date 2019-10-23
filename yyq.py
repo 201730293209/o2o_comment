1ac5a79fc21ef25124089a682ea82f0799e6d6c7
Your Name <email@exampl.com>
2019-10-23 09:56:07 +0800

    排除向量和特征词的问题

--- yyq.py
+++ yyq.py
@@ -81,13 +81,32 @@ def delete_words(all_words_list, delete_num=100):
             stopwords_set.add(stopword)
     # -------------------------------------------------------
     feature_words = []  # 特征词，即有效的词
+    n=1
     for t in range(delete_num, len(all_words_list), 1):
+        if n>1000:
+            break
         # 如果这个词不是数字，并且不是停用词，且单词长度大于1小于5，那么这个词就可以作为特征词
         if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1 < len(
                 all_words_list[t]) < 5:
             feature_words.append(all_words_list[t])
+        n+=1
     return feature_words
 
+def create_words_vec(comment_list, feature_words):
+    '''
+    函数说明：生成词条（句子）向量
+    :param comment_list: 评论列表
+    :param feature_words: 特征词
+    :return: words_vec: 词条向量，采用one-hot热编码方式
+    '''
+    words_vec = []   #词条向量
+    for comment in comment_list:  #取出每条评论
+        temp_vec = [0] * len(feature_words)    #生成和feature_words相同长度的词向量
+        for word in comment:  #取出评论中的每个词
+            if word in feature_words:   #如果该词在features_words（词汇表）中出现
+                temp_vec[feature_words.index(word)] = 1    #则在对应位置记1
+        words_vec.append(temp_vec)
+    return words_vec
 
 def TextFeatures(train_data_list, test_data_list, feature_words):
     """
@@ -113,17 +132,16 @@ def TextFeatures(train_data_list, test_data_list, feature_words):
 def TextClassifier(train_list, test_list, train_label, test_label):
     '''
     函数说明：文本分类器，计算精确度
-    :param train_list: 将向量化之后的训练集的已切分comment传入
-    :param test_list: 将向量化后的测试集的已切分的comment传入
-    :return: test_label: 用多重贝叶斯预测出的测试集的label值
+    :parameters：
+        train_list: 将向量化之后的训练集的已切分comment传入
+        test_list: 将向量化后的测试集的已切分的comment传入
+    :return:
+        test_label: 用多重贝叶斯预测出的测试集的label值
     '''
     classifier = MultinomialNB().partial_fit(train_list, train_label, classes = np.array([0, 1]))
     test_label = classifier.predict(test_list)
     # test_accuracy = classifier.score(test_list, test_label)
     return test_label
-
-
-
 
 
 if __name__ == '__main__':
@@ -135,9 +153,11 @@ if __name__ == '__main__':
     #----------词频排序与特征词选择------------#
     all_words_list = sort_by_frequency(train_comment_list)
     feature_words = delete_words(all_words_list)
-    
+
     # ----------将训练集和测试集向量化------------#
-    train_feature_list, test_feature_list = TextFeatures(train_comment_list, test_comment_list, feature_words)
+    #train_feature_list, test_feature_list = TextFeatures(train_comment_list, test_comment_list, feature_words)
+    train_feature_list=create_words_vec(train_comment_list,feature_words)
+    test_feature_list=create_words_vec(test_comment_list,feature_words)
 
     # ----------运用贝叶斯将测试集的label值预测出来------------#
     test_label = TextClassifier(train_feature_list, test_feature_list,  train_label_list, train_label_list)
